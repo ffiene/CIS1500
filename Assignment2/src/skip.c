8e8927a68fe03b979c8aebc9beb7f39c5d4fe001
ffiene <ffiene@gmail.com>
2021-03-01 01:36:03 -0500

    split input validation if-else

--- Assignment2/src/skip.c
+++ Assignment2/src/skip.c
@@ -1,7 +1,7 @@
 /*
-Jennifer Fiene
-jfiene@uoguelph.ca
-*/
+ Jennifer Fiene
+ jfiene@uoguelph.ca
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,13 +33,19 @@ int main() {
   if (skip < 0 && end > start) {
     printf("Error: If skip number is negative, start must be less than the end number.\n");
     return 1;
-  } else if (skip > 0 && end < start) {
+  }
+
+  if (skip > 0 && end < start) {
     printf("Error: If skip number is positive, start must be greater than the end number.\n");
     return 1;
-  } else if (skip == 0) {
+  }
+
+  if (skip == 0) {
     printf("Error: Skip number must not be zero.\n");
     return 1;
-  } else if (absDifference <= skip) {
+  }
+
+  if (absDifference <= skip) {
     printf("Error: Difference between start and end numbers must be less than the skip value.\n");
     return 1;
   }
@@ -49,11 +55,12 @@ int main() {
   if (start < end) {
     printf("start less than end");
   }
+
   /*
-  For loop that adds the skip number to the start value until the end
-  value is reached. Prints the current number each time it does this.
-  */
-  for (int i = start; i < end; i = i + (skip)){
+   For loop that adds the skip number to the start value until the end
+   value is reached. Prints the current number each time it does this.
+   */
+  for (int i = start; i < end; i = i + (skip)) {
     printf("For loop started");
     printf("%d\n", i);
   }
